9c8c7904de430c842798c0ed7665d87f1dd0a05c
kim.tran <kim.tran@capgemini.com>
2024-04-03 10:20:50 +0200

    feat: update libs and chart

--- webpack.config.js
+++ webpack.config.js
@@ -6,62 +6,6 @@ const {
   withModuleFederationPlugin,
 } = require('@angular-architects/module-federation/webpack');
 
-// const config = withModuleFederationPlugin({
-//   shared: share({
-//     '@angular/core': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     '@angular/forms': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     '@angular/common': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: {
-//         skip: ['@angular/common/http/testing'],
-//       },
-//       // eager: true
-//     },
-//     '@angular/common/http': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     '@angular/router': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     rxjs: {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     '@onecx/portal-integration-angular': {
-//       requiredVersion: 'auto',
-//       includeSecondaries: true,
-//       // eager: true
-//     },
-//     // '@onecx/shell-core': {
-//     //   requiredVersion: 'auto',
-//     //   includeSecondaries: true,
-//     // },
-//     '@ngx-translate/core': {
-//       singleton: true,
-//       strictVersion: false,
-//       requiredVersion: '^14.0.0',
-//       // eager: true
-//     },
-//   }),
-
-//   sharedMappings: ['@onecx/portal-integration-angular'],
-// });
-
-// module.exports = config;
-
 const webpackConfig = {
   ...withModuleFederationPlugin({
     shared: share({

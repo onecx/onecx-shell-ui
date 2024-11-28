72040553d63e86c1273b51c5f578fac65e9b3914
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2024-11-28 12:10:59 +0100

    fix: set shell name to default value

--- src/app/shell/components/not-found-page.component.ts
+++ src/app/shell/components/not-found-page.component.ts
@@ -23,7 +23,7 @@ export class PageNotFoundComponent {
       baseHref: '/',
       mountPath: '',
       remoteBaseUrl: '',
-      shellName: '',
+      shellName: 'portal',
       productName: ''
     })
     this.workspace = this.appStateService.currentWorkspace$.getValue()

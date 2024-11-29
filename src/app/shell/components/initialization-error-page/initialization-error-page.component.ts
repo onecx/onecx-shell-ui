3825b56967949c447157af7eadcdccb8e4e9522b
Henry Taeschner <henry.taeschner@capgemini.com>
2024-11-29 08:32:03 +0100

    feat: tune layout of error pages

--- src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
+++ src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
@@ -1,51 +1,53 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SerializedInitializationError } from '../../utils/initialization-error-handler.utils';
+import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { SerializedInitializationError } from '../../utils/initialization-error-handler.utils'
 
 @Component({
   template: `<div class="p-4">
-    <h1 class="md:text-4xl text-2xl mb-1">{{ 'INITIALIZATION_ERROR_PAGE.TITLE' | translate }}</h1>
-    <p class="md:text-2xl text-lg mb-1">{{ 'INITIALIZATION_ERROR_PAGE.SUBTITLE' | translate }}</p>
-    <div *ngIf="error.message">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.MESSAGE' | translate }}
-        {{ error.message }}
-      </p>
+    <h1 class="md:text-2xl text-lg mb-1">{{ 'INITIALIZATION_ERROR_PAGE.TITLE' | translate }}</h1>
+    <p class="md:text-lg text-base mb-1">{{ 'INITIALIZATION_ERROR_PAGE.SUBTITLE' | translate }}</p>
+    <div class="flex flex-column row-gap-2">
+      <ng-container *ngIf="error.message">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.MESSAGE' | translate }}
+        </div>
+        <i>{{ error.message }}</i>
+      </ng-container>
+      <ng-container *ngIf="error.requestedUrl">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.REQUESTED_URL' | translate }}
+          <i>{{ error.requestedUrl }}</i>
+        </div>
+      </ng-container>
+      <ng-container *ngIf="error.detail">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.DETAILS' | translate }}
+          <i>{{ error.detail }}</i>
+        </div>
+      </ng-container>
+      <ng-container *ngIf="error.errorCode">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.ERRORCODE' | translate }}
+          <i>{{ error.errorCode }}</i>
+        </div>
+      </ng-container>
+      <ng-container *ngIf="error.invalidParams">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.INVALID_PARAMS' | translate }}
+          <i>{{ error.invalidParams }}</i>
+        </div>
+      </ng-container>
+      <ng-container *ngIf="error.params">
+        <div class="md:text-base text-sm">
+          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.PARAMS' | translate }}
+          <i>{{ error.params }}</i>
+        </div>
+      </ng-container>
     </div>
-    <div *ngIf="error.requestedUrl">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.REQUESTED_URL' | translate }}
-        {{ error.requestedUrl }}
-      </p>
-    </div>
-    <div *ngIf="error.detail">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.DETAILS' | translate }}
-        {{ error.detail }}
-      </p>
-    </div>
-    <div *ngIf="error.errorCode">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.ERRORCODE' | translate }}
-        {{ error.errorCode }}
-      </p>
-    </div>
-    <div *ngIf="error.invalidParams">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.INVALID_PARAMS' | translate }}
-        {{ error.invalidParams }}
-      </p>
-    </div>
-    <div *ngIf="error.params">
-      <p class="md:text-lg text-sm">
-        {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.PARAMS' | translate }}
-        {{ error.params }}
-      </p>
-    </div>
-  </div> `,
+  </div> `
 })
 export class InitializationErrorPageComponent {
-  error: SerializedInitializationError;
+  error: SerializedInitializationError
   constructor(private route: ActivatedRoute) {
     this.error = {
       message: this.route.snapshot.paramMap.get('message') ?? '',
@@ -53,7 +55,7 @@ export class InitializationErrorPageComponent {
       detail: this.route.snapshot.paramMap.get('detail') ?? '',
       errorCode: this.route.snapshot.paramMap.get('errorCode') ?? '',
       params: this.route.snapshot.paramMap.get('params') ?? '',
-      invalidParams: this.route.snapshot.paramMap.get('invalidParams') ?? '',
-    };
+      invalidParams: this.route.snapshot.paramMap.get('invalidParams') ?? ''
+    }
   }
 }

bda5ef42bc092dd236dfa86a31af120ce40c9d01
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-01-03 09:10:47 +0100

    feat: add onecx watermark (#453)
    * feat: add onecx watermark, fix translations
    
    * feat: add husky
    
    * feat: add favicon
    
    * fix: adjust text size on error pages
    
    * fix: tuning error pages
    
    * fix: tuning error pages 2
    
    * fix: added missed html
    
    * fix: code smells
    
    * fix: code smells
    
    * fix: code smells

--- src/app/shell/components/error-page.component.ts
+++ src/app/shell/components/error-page.component.ts
@@ -7,12 +7,24 @@ import { getLocation } from '@onecx/accelerator'
   template: `
     <div class="p-4 flex flex-column gap-5">
       <div>
-        <h1 class="md:text-2xl text-lg">{{ 'ERROR_PAGE.TITLE' | translate }}</h1>
-        <p class="md:text-lg text-base mb-1">{{ 'ERROR_PAGE.DETAILS' | translate }}</p>
-        <p class="md:text-base text-sm">{{ 'ERROR_PAGE.REQUESTED_PAGE' | translate }} {{ requestedApplicationPath }}</p>
+        <h1 class="md:text-xl text-lg">{{ 'ERROR_PAGE.TITLE' | translate }}</h1>
+        <p class="mb-1">{{ 'ERROR_PAGE.DETAILS' | translate }}</p>
+        <p>
+          <span class="font-bold"> {{ 'ERROR_PAGE.REQUESTED_PAGE' | translate }} </span>
+          <i>{{ requestedApplicationPath }}</i>
+        </p>
       </div>
-      <button pButton (click)="reloadPage()" routerLinkActive="router-link-active" class="w-max">
-        {{ 'ERROR_PAGE.BUTTON' | translate }}
+      <button
+        pButton
+        (click)="reloadPage()"
+        routerLinkActive="router-link-active"
+        class="w-max"
+        [ariaLabel]="'ERROR_PAGE.ACTION' | translate"
+        [pTooltip]="'ERROR_PAGE.ACTION.TOOLTIP' | translate"
+        tooltipPosition="top"
+        tooltipEvent="hover"
+      >
+        {{ 'ERROR_PAGE.ACTION' | translate }}
       </button>
     </div>
   `
--- src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
+++ src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
@@ -12,48 +12,7 @@ interface InitializationError {
 }
 
 @Component({
-  template: `<div class="p-4">
-    <h1 class="md:text-2xl text-lg mb-1">{{ 'INITIALIZATION_ERROR_PAGE.TITLE' | translate }}</h1>
-    <p class="md:text-lg text-base">{{ 'INITIALIZATION_ERROR_PAGE.SUBTITLE' | translate }}</p>
-    <div *ngIf="error$ | async as error" class="mt-3 flex flex-column row-gap-2">
-      <div id="onecxInitializationErrorMessage" *ngIf="error.message">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.MESSAGE' | translate }}
-        </div>
-        <i>{{ error.message }}</i>
-      </div>
-      <div id="onecxInitializationErrorRequestedUrl" *ngIf="error.requestedUrl">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.REQUESTED_URL' | translate }}
-        </div>
-        <i>{{ error.requestedUrl }}</i>
-      </div>
-      <div id="onecxInitializationErrorDetail" *ngIf="error.detail">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.DETAILS' | translate }}
-        </div>
-        <i>{{ error.detail }}</i>
-      </div>
-      <div id="onecxInitializationErrorErrorCode" *ngIf="error.errorCode">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.ERRORCODE' | translate }}
-        </div>
-        <i>{{ error.errorCode }}</i>
-      </div>
-      <div id="onecxInitializationErrorInvalidParams" *ngIf="error.invalidParams">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.INVALID_PARAMS' | translate }}
-        </div>
-        <i>{{ error.invalidParams }}</i>
-      </div>
-      <div id="onecxInitializationErrorParams" *ngIf="error.params">
-        <div class="md:text-base text-sm">
-          {{ 'INITIALIZATION_ERROR_PAGE.DETAILS.PARAMS' | translate }}
-        </div>
-        <i>{{ error.params }}</i>
-      </div>
-    </div>
-  </div> `
+  templateUrl: './initialization-error-page.component.html'
 })
 export class InitializationErrorPageComponent {
   error$: Observable<InitializationError>

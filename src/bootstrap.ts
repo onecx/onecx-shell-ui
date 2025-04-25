4fc3708089683c41526fbb837373938b6a68def3
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-04-25 13:27:58 +0200

    fix: fix wrong import

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,7 @@
 import { bootstrapModule } from '@onecx/angular-webcomponents'
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
-import { Capability, ShellCapabilityService } from '@onecx/angular-utils'
+import { Capability, ShellCapabilityService } from '@onecx/angular-integration-interface'
 
 ShellCapabilityService.setCapabilities([Capability.CURRENT_LOCATION_TOPIC])
 bootstrapModule(AppModule, 'shell', environment.production)

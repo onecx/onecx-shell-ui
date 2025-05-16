366a0475f0c9e1bcaf97d4b7512c1d215dcce7ba
Henry Taeschner <github@henry-taeschner.de>
2025-05-16 12:10:38 +0200

    feat: add logout action on page for initialization failure

--- src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
+++ src/app/shell/components/initialization-error-page/initialization-error-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Observable, map } from 'rxjs'
 
+import { EventsPublisher } from '@onecx/integration-interface'
+
 interface InitializationError {
   message: string
   requestedUrl: string
@@ -16,11 +18,14 @@ interface InitializationError {
 })
 export class InitializationErrorPageComponent {
   error$: Observable<InitializationError>
+  public eventsPublisher$: EventsPublisher = new EventsPublisher()
 
   constructor(private readonly route: ActivatedRoute) {
     this.error$ = this.route.fragment.pipe(
       map((fragment) => {
         const params = new URLSearchParams(fragment ?? '')
+        // console.log('fragment', fragment)
+        // console.log('requestedUrl', params.get('requestedUrl'))
         return {
           message: params.get('message') ?? '',
           requestedUrl: params.get('requestedUrl') ?? '',
@@ -31,5 +36,14 @@ export class InitializationErrorPageComponent {
         }
       })
     )
+  }
+  /*
+  http://localhost:5000/portal-initialization-error-page#
+  message=Http+failure+response+for+http%253A%252F%252Flocalhost%253A5000%252Fshell-bff%252FworkspaceConfig%253A+404+Not+Found&requestedUrl=http%253A%252F%252Flocalhost%253A5000%252Fportal-initialization-error-page%2523
+  
+  message%253DHttp%252Bfailure%252Bresponse%252Bfor%252Bhttp%2525253A%2525252F%2525252Flocalhost%2525253A5000%2525252Fshell-bff%2525252FworkspaceConfig%2525253A%252B404%252BNot%252BFound%2526requestedUrl%253Dhttp%2525253A%2525252F%2525252Flocalhost%2525253A5000%2525252Fadmi%2526detail%253D%2526errorCode%253D%2526invalidParams%253D%2526params%253D&detail=&errorCode=&invalidParams=&params=
+  */
+  public onLogout(): void {
+    this.eventsPublisher$.publish({ type: 'authentication#logoutButtonClicked' })
   }
 }

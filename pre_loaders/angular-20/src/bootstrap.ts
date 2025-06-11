31b90c636220df4c4ee1d979195d2a7a192bf479
markuczy <129275100+markuczy@users.noreply.github.com>
2025-06-11 08:55:31 +0200

    fix: remove libs reference from preloader

--- pre_loaders/angular-20/src/bootstrap.ts
+++ pre_loaders/angular-20/src/bootstrap.ts
@@ -1,5 +1,32 @@
+import { NgZone, PlatformRef, VERSION, Version, enableProdMode, getPlatform } from '@angular/core'
 import { PreloaderModule } from './bootstrap-utils'
-import { cachePlatform, getNgZone } from '@onecx/angular-webcomponents'
+import { platformBrowser } from '@angular/platform-browser'
+
+function cachePlatform(production: boolean): PlatformRef {
+  let platformCache: Map<Version, PlatformRef> = getWindowState().platformCache
+  if (!platformCache) {
+    platformCache = new Map<Version, PlatformRef>()
+    getWindowState().platformCache = platformCache
+  }
+  const version = VERSION
+  let platform: any = platformCache.get(version)
+  if (!platform) {
+    platform = getPlatform() ?? platformBrowser()
+    platform && platformCache.set(version, platform)
+    production ?? enableProdMode()
+  }
+  return platform
+}
+
+function getWindowState(): any {
+  const state = window as any
+  state['@onecx/angular-webcomponents'] ??= {} as unknown
+  return state['@onecx/angular-webcomponents']
+}
+
+function getNgZone(): NgZone {
+  return getWindowState().ngZone
+}
 
 cachePlatform(true)
   .bootstrapModule(PreloaderModule, {

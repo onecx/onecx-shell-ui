8bf67b1a1e62ec4a45ff87a05649ccf86590158f
markuczy <129275100+markuczy@users.noreply.github.com>
2025-06-30 10:07:23 +0200

    fix: report status of building preloaders

--- build-preloaders-parallel.sh
+++ build-preloaders-parallel.sh
@@ -2,30 +2,31 @@
 
 echo "Started building preloaders"
 
-# Define log files
 LOG18="build-angular-18.log"
 LOG19="build-angular-19.log"
 LOG20="build-angular-20.log"
 
-# Remove old logs if they exist
 rm -f $LOG18 $LOG19 $LOG20
 
 # Run builds in parallel and redirect output to log files
-(cd ./pre_loaders/angular-18 && npm run build) > $LOG18 2>&1 & 
+(cd ./pre_loaders/angular-18 && npm run build) > $LOG18 2>&1 &
 PID1=$!
 
-(cd ./pre_loaders/angular-19 && npm run build) > $LOG19 2>&1 & 
+(cd ./pre_loaders/angular-19 && npm run build) > $LOG19 2>&1 &
 PID2=$!
 
-(cd ./pre_loaders/angular-20 && npm run build) > $LOG20 2>&1 & 
+(cd ./pre_loaders/angular-20 && npm run build) > $LOG20 2>&1 &
 PID3=$!
 
-# Wait for all builds to complete
 wait $PID1
-wait $PID2
-wait $PID3
+STATUS1=$?
 
-# Print logs sequentially
+wait $PID2
+STATUS2=$?
+
+wait $PID3
+STATUS3=$?
+
 echo "Build Log: Angular 18"
 cat $LOG18
 echo
@@ -38,4 +39,10 @@ echo "Build Log: Angular 20"
 cat $LOG20
 echo
 
-echo "All builds completed."
+# Check if any build failed
+if [[ $STATUS1 -ne 0 || $STATUS2 -ne 0 || $STATUS3 -ne 0 ]]; then
+  echo "One or more builds failed."
+  exit 1
+else
+  echo "All builds completed successfully."
+fi

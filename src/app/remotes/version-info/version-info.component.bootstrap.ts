25fe8c70012fe59aa6a242f5f43336af7f82715e
Henry Taeschner <github@henry-taeschner.de>
2025-07-21 13:01:52 +0200

    fix: migrate version RC to lib v6

--- src/app/remotes/version-info/version-info.component.bootstrap.ts
+++ src/app/remotes/version-info/version-info.component.bootstrap.ts
@@ -1,24 +1,33 @@
-import { importProvidersFrom } from '@angular/core'
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { BrowserModule } from '@angular/platform-browser'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { provideRouter } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
+import { TranslateLoader } from '@ngx-translate/core'
+import { ReplaySubject } from 'rxjs'
 
-import { AngularAuthModule } from '@onecx/angular-auth'
 import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
-
+import {
+  REMOTE_COMPONENT_CONFIG,
+  RemoteComponentConfig,
+  provideTranslateServiceForRoot
+} from '@onecx/angular-remote-components'
+import { TRANSLATION_PATH, createTranslateLoader, remoteComponentTranslationPathFactory } from '@onecx/angular-utils'
 import { environment } from 'src/environments/environment'
+
 import { OneCXVersionInfoComponent } from './version-info.component'
 
 bootstrapRemoteComponent(OneCXVersionInfoComponent, 'ocx-version-info-component', environment.production, [
-  provideHttpClient(withInterceptorsFromDi()),
-  importProvidersFrom(AngularAuthModule),
-  importProvidersFrom(BrowserModule),
-  importProvidersFrom(BrowserAnimationsModule),
-  provideRouter([
-    {
-      path: '**',
-      children: []
+  { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<RemoteComponentConfig>(1) },
+  provideTranslateServiceForRoot({
+    isolate: true,
+    loader: {
+      provide: TranslateLoader,
+      useFactory: createTranslateLoader,
+      deps: [HttpClient]
     }
-  ])
+  }),
+  {
+    provide: TRANSLATION_PATH,
+    useFactory: (remoteComponentConfig: ReplaySubject<RemoteComponentConfig>) =>
+      remoteComponentTranslationPathFactory('assets/i18n/')(remoteComponentConfig),
+    multi: true,
+    deps: [REMOTE_COMPONENT_CONFIG]
+  }
 ])
--- src/app/remotes/version-info/version-info.component.ts
+++ src/app/remotes/version-info/version-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HttpClient } from '@angular/common/http'
 import { UntilDestroy } from '@ngneat/until-destroy'
@@ -7,15 +7,15 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
 import {
   AngularRemoteComponentsModule,
-  BASE_URL,
+  REMOTE_COMPONENT_CONFIG,
   RemoteComponentConfig,
   ocxRemoteComponent,
   ocxRemoteWebcomponent,
   provideTranslateServiceForRoot
 } from '@onecx/angular-remote-components'
-import { createRemoteComponentTranslateLoader } from '@onecx/angular-accelerator'
+import { AngularAcceleratorModule } from '@onecx/angular-accelerator'
 import { AppStateService, CONFIG_KEY, ConfigurationService } from '@onecx/angular-integration-interface'
-import { PortalCoreModule } from '@onecx/portal-integration-angular'
+import { remoteComponentTranslationPathFactory } from '@onecx/angular-utils'
 
 type Version = {
   workspaceName: string
@@ -28,15 +28,15 @@ type Version = {
   selector: 'ocx-shell-version-info',
   templateUrl: './version-info.component.html',
   standalone: true,
-  imports: [AngularRemoteComponentsModule, CommonModule, PortalCoreModule, TranslateModule],
+  imports: [AngularRemoteComponentsModule, CommonModule, TranslateModule, AngularAcceleratorModule],
   providers: [
-    { provide: BASE_URL, useValue: new ReplaySubject<string>(1) },
+    { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<string>(1) },
     provideTranslateServiceForRoot({
       isolate: true,
       loader: {
         provide: TranslateLoader,
-        useFactory: createRemoteComponentTranslateLoader,
-        deps: [HttpClient, BASE_URL]
+        useFactory: remoteComponentTranslationPathFactory,
+        deps: [HttpClient, REMOTE_COMPONENT_CONFIG]
       }
     })
   ],
@@ -44,26 +44,27 @@ type Version = {
 })
 @UntilDestroy()
 export class OneCXVersionInfoComponent implements ocxRemoteComponent, ocxRemoteWebcomponent {
+  private readonly rcConfig = inject<ReplaySubject<string>>(REMOTE_COMPONENT_CONFIG)
+  readonly configurationService = inject(ConfigurationService)
+  private readonly appState = inject(AppStateService)
+
   @Input() set ocxRemoteComponentConfig(config: RemoteComponentConfig) {
     this.ocxInitRemoteComponent(config)
   }
 
   public versionInfo$!: Observable<Version | undefined>
 
-  constructor(
-    @Inject(BASE_URL) private readonly baseUrl: ReplaySubject<string>,
-    public readonly configurationService: ConfigurationService,
-    private readonly appState: AppStateService
-  ) {
+  constructor() {
     this.versionInfo$ = combineLatest([
       this.appState.currentMfe$.asObservable(),
       this.appState.currentWorkspace$.asObservable(),
+      this.configurationService.getProperty(CONFIG_KEY.APP_VERSION),
       from(this.configurationService.isInitialized)
     ]).pipe(
-      map(([mfe, workspace]) => {
+      map(([mfe, workspace, hostVersion]) => {
         const version: Version = { workspaceName: workspace.workspaceName }
         const mfeInfoVersion = mfe.version ? ' ' + mfe.version : ''
-        version.hostVersion = this.configurationService.getProperty(CONFIG_KEY.APP_VERSION) ?? ''
+        version.hostVersion = hostVersion
         version.separator = mfe.displayName || mfeInfoVersion !== '' ? ' - ' : ''
         version.mfeInfo = mfe.displayName ? mfe.displayName + mfeInfoVersion : ''
         return version
@@ -72,6 +73,6 @@ export class OneCXVersionInfoComponent implements ocxRemoteComponent, ocxRemoteW
   }
 
   public ocxInitRemoteComponent(remoteComponentConfig: RemoteComponentConfig) {
-    this.baseUrl.next(remoteComponentConfig.baseUrl)
+    this.rcConfig.next(remoteComponentConfig.baseUrl)
   }
 }

a9f395f8361b3d8fadba1c5f72901d724006b60c
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-07-25 08:38:06 +0200

    fix: use provideAppInitializer() correctly (#673)

--- src/app/remotes/version-info/version-info.component.ts
+++ src/app/remotes/version-info/version-info.component.ts
@@ -1,20 +1,13 @@
 import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { HttpClient } from '@angular/common/http'
 import { UntilDestroy } from '@ngneat/until-destroy'
 import { combineLatest, from, map, Observable, ReplaySubject } from 'rxjs'
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
-import {
-  REMOTE_COMPONENT_CONFIG,
-  RemoteComponentConfig,
-  remoteComponentTranslationPathFactory
-} from '@onecx/angular-utils'
+import { REMOTE_COMPONENT_CONFIG, RemoteComponentConfig } from '@onecx/angular-utils'
 import {
   AngularRemoteComponentsModule,
   ocxRemoteComponent,
-  ocxRemoteWebcomponent,
-  provideTranslateServiceForRoot
+  ocxRemoteWebcomponent
 } from '@onecx/angular-remote-components'
 import { AngularAcceleratorModule } from '@onecx/angular-accelerator'
 import { AppStateService, CONFIG_KEY, ConfigurationService } from '@onecx/angular-integration-interface'
@@ -30,18 +23,8 @@ export type Version = {
   selector: 'ocx-shell-version-info',
   templateUrl: './version-info.component.html',
   standalone: true,
-  imports: [AngularRemoteComponentsModule, CommonModule, TranslateModule, AngularAcceleratorModule],
-  providers: [
-    { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<string>(1) },
-    provideTranslateServiceForRoot({
-      isolate: true,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: remoteComponentTranslationPathFactory,
-        deps: [HttpClient, REMOTE_COMPONENT_CONFIG]
-      }
-    })
-  ],
+  imports: [AngularRemoteComponentsModule, CommonModule, AngularAcceleratorModule],
+  providers: [{ provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<string>(1) }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 @UntilDestroy()
@@ -54,28 +37,24 @@ export class OneCXVersionInfoComponent implements ocxRemoteComponent, ocxRemoteW
     this.ocxInitRemoteComponent(rcConfig)
   }
 
-  public versionInfo$: Observable<Version | undefined>
-
-  constructor() {
-    this.versionInfo$ = combineLatest([
-      this.appState.currentMfe$.asObservable(),
-      this.appState.currentWorkspace$.asObservable(),
-      this.config.getProperty(CONFIG_KEY.APP_VERSION),
-      from(this.config.isInitialized)
-    ]).pipe(
-      map(([mfe, workspace, hostVersion]) => {
-        const mfeVersion = mfe.version ?? ''
-        const mfeInfo = mfe.displayName + (mfe.version ? ' ' + mfeVersion : '')
-        const version: Version = {
-          workspaceName: workspace.workspaceName,
-          shellInfo: hostVersion,
-          mfeInfo: mfe.displayName ? mfeInfo : '',
-          separator: mfe.displayName || mfe.version ? ' - ' : ''
-        }
-        return version
-      })
-    )
-  }
+  public versionInfo$: Observable<Version | undefined> = combineLatest([
+    this.appState.currentMfe$.asObservable(),
+    this.appState.currentWorkspace$.asObservable(),
+    this.config.getProperty(CONFIG_KEY.APP_VERSION),
+    from(this.config.isInitialized)
+  ]).pipe(
+    map(([mfe, workspace, hostVersion]) => {
+      const mfeVersion = mfe.version ?? ''
+      const mfeInfo = mfe.displayName + (mfe.version ? ' ' + mfeVersion : '')
+      const version: Version = {
+        workspaceName: workspace.workspaceName,
+        shellInfo: hostVersion,
+        mfeInfo: mfe.displayName ? mfeInfo : '',
+        separator: mfe.displayName || mfe.version ? ' - ' : ''
+      }
+      return version
+    })
+  )
 
   public ocxInitRemoteComponent(rcConfig: RemoteComponentConfig) {
     this.rcConfig.next(rcConfig)

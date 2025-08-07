9eedd251b269198142590d292d0edf018e1efb5c
markuczy <129275100+markuczy@users.noreply.github.com>
2025-08-07 09:30:01 +0200

    feat: dynamic content should have style id more frequently (#679)
    * feat: dynamic content should have style id more frequently
    
    * feat: adjusted based on review
    
    * fix: code smell

---
+++ src/app/shell/utils/styles/angular-material-overwrites.utils.ts
@@ -0,0 +1,15 @@
+import { getOnecxTriggerElement } from './onecx-trigger-element.utils'
+import { appendIntermediateStyleData, getStyleDataOrIntermediateStyleData } from './style-data.utils'
+
+// When creating elements in Angular Material make sure to include the style id data in them so when appending to the body we don't lose context of the current App
+export function ensureMaterialDynamicDataIncludesIntermediateStyleData() {
+  // eslint-disable-next-line @typescript-eslint/no-extra-semi
+  ;(document as any).createElementFromMaterial = function (context: any, tagName: any, options?: any): HTMLElement {
+    const el = document.createElement(tagName, options)
+    const onecxTrigger = getOnecxTriggerElement()
+    const styleData = onecxTrigger ? getStyleDataOrIntermediateStyleData(onecxTrigger) : null
+    // Append intermediate data so the isolation does not happen by coincidence
+    styleData && appendIntermediateStyleData(el, styleData)
+    return el
+  }
+}
---
+++ src/app/shell/utils/styles/primeng-overwrites.utils.ts
@@ -0,0 +1,18 @@
+import { getOnecxTriggerElement } from './onecx-trigger-element.utils'
+import { appendIntermediateStyleData, getStyleDataOrIntermediateStyleData } from './style-data.utils'
+
+// When creating elements in PrimeNg make sure to include the style id data in them so when appending to the body we don't lose context of the current App
+export function ensurePrimengDynamicDataIncludesIntermediateStyleData() {
+  // eslint-disable-next-line @typescript-eslint/no-extra-semi
+  ;(document as any).createElementFromPrimeNg = function (context: any, tagName: any, options?: any): HTMLElement {
+    const el = document.createElement(tagName, options)
+    const contextElement = context['this']?.el?.nativeElement
+    const onecxTrigger = getOnecxTriggerElement()
+    const styleData =
+      (contextElement ? getStyleDataOrIntermediateStyleData(contextElement) : null) ??
+      (onecxTrigger ? getStyleDataOrIntermediateStyleData(onecxTrigger) : null)
+    // Append intermediate data so the isolation does not happen by coincidence
+    styleData && appendIntermediateStyleData(el, styleData)
+    return el
+  }
+}

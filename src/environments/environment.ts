f9deac2a33a169bde40c5aac82288cb730b15c97
Henry Taeschner <github@henry-taeschner.de>
2025-08-21 19:44:16 +0200

    fix: use libs v6.5.3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  KEYCLOAK_CLIENT_ID: 'onecx-shell-ui-client',
+  KEYCLOAK_CLIENT_ID: 'portal-mf-shell',
   KEYCLOAK_URL: 'http://keycloak-app/',
-  KEYCLOAK_REALM: 'onecx',
+  KEYCLOAK_REALM: 'OneCX',
   skipRemoteConfigLoad: true,
   production: false,
   APP_VERSION: 'Local Shell Version'

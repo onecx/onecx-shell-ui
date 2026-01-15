33f233ae68144f726a086fcbe91b5bcf473c093b
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-01-15 12:21:16 +0100

    refactor: share scope

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const { ModifyEntryPlugin } = require('@angular-architects/module-federation/src
 const { share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack')
 const { ModifySourcePlugin, ReplaceOperation } = require('modify-source-webpack-plugin')
 
-const angular20Scope = 'angular-20'
+const shareScope = 'angular-20'
 
 const webpackConfig = {
   ...withModuleFederationPlugin({
@@ -12,31 +12,31 @@ const webpackConfig = {
       './OneCXShellToastComponent': 'src/app/remotes/shell-toast/shell-toast.component.main.ts'
     },
     shared: share({
-      '@angular/core': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
+      '@angular/core': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
       '@angular/common': {
         requiredVersion: 'auto',
         includeSecondaries: { skip: ['@angular/common/http/testing'] },
-        shareScope: angular20Scope 
+        shareScope 
       },
-      '@angular/common/http': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@angular/elements': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@angular/forms': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@angular/platform-browser': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@angular/router': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@angular-architects/module-federation-tools': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@ngx-translate/core': { requiredVersion: 'auto', shareScope: angular20Scope },
-      '@ngx-translate/http-loader': { requiredVersion: 'auto', shareScope: angular20Scope },
-      primeng: { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      rxjs: { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-accelerator': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-auth': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-integration-interface': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-remote-components': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-utils': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/angular-webcomponents': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope },
-      '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true, shareScope: angular20Scope }
+      '@angular/common/http': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@angular/elements': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@angular/forms': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@angular/platform-browser': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@angular/router': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@angular-architects/module-federation-tools': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@ngx-translate/core': { requiredVersion: 'auto', shareScope },
+      '@ngx-translate/http-loader': { requiredVersion: 'auto', shareScope },
+      primeng: { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      rxjs: { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-accelerator': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-auth': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-integration-interface': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-remote-components': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-utils': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/angular-webcomponents': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true, shareScope },
+      '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true, shareScope }
     })
   })
 }

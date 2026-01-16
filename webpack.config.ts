2efbe63b9684354cb25118d82af63c3215869602
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-01-16 13:24:38 +0100

    feat: webpack config with plugins and import meta

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,47 +1,63 @@
+import { Configuration } from 'webpack'
 import config from './module-federation.config'
 import { withModuleFederation } from '@nx/angular/module-federation'
+import { ModifySourcePlugin, ReplaceOperation } from 'modify-source-webpack-plugin'
 
-export default withModuleFederation(config)
+const modifyPrimeNgPlugin = new ModifySourcePlugin({
+  rules: [
+    {
+      test: (module) => {
+        if (module.resource) {
+          return module.resource.includes('primeng')
+        }
+        return false
+      },
+      operations: [
+        new ReplaceOperation(
+          'all',
+          'document\\.createElement\\(',
+          'document.createElementFromPrimeNg({"this": this, "arguments": Array.from(arguments)},'
+        ),
+        new ReplaceOperation('all', 'Theme.setLoadedStyleName', '(function(_){})')
+      ]
+    }
+  ]
+})
 
-// const plugins = webpackConfig.plugins.filter((plugin) => !(plugin instanceof ModifyEntryPlugin))
-// const modifyPrimeNgPlugin = new ModifySourcePlugin({
-//   rules: [
-//     {
-//       test: (module) => {
-//         return module.resource && module.resource.includes('primeng')
-//       },
-//       operations: [
-//         new ReplaceOperation(
-//           'all',
-//           'document\\.createElement\\(',
-//           'document.createElementFromPrimeNg({"this": this, "arguments": Array.from(arguments)},'
-//         ),
-//         new ReplaceOperation('all', 'Theme.setLoadedStyleName', '(function(_){})')
-//       ]
-//     }
-//   ]
-// })
+// Replace createElement only in @angular/platform-browser SharedStylesHost
+const modifyAngularCorePlugin = new ModifySourcePlugin({
+  rules: [
+    {
+      test: (module) => {
+        if (module.resource) {
+          return module.resource.includes('@angular/platform-browser')
+        }
+        return false
+      },
+      operations: [
+        new ReplaceOperation(
+          'all',
+          "this\\.doc\\.createElement\\(\\'style\\'",
+          "this.doc.createElementFromSharedStylesHost({'this': this, 'arguments': Array.from(arguments)},'style'"
+        )
+      ]
+    }
+  ]
+})
 
-// // Replace createElement only in @angular/platform-browser SharedStylesHost
-// const modifyAngularCorePlugin = new ModifySourcePlugin({
-//   rules: [
-//     {
-//       test: (module) => {
-//         return module.resource && module.resource.includes('@angular/platform-browser')
-//       },
-//       operations: [
-//         new ReplaceOperation(
-//           'all',
-//           "this\\.doc\\.createElement\\(\\'style\\'",
-//           "this.doc.createElementFromSharedStylesHost({'this': this, 'arguments': Array.from(arguments)},'style'"
-//         )
-//       ]
-//     }
-//   ]
-// })
+export default async function (baseConfig: Configuration) {
+  const withMf = await withModuleFederation(config)
+  const webpackConfig = withMf(baseConfig)
 
-// module.exports = {
-//   ...webpackConfig,
-//   plugins: [...plugins, modifyPrimeNgPlugin, modifyAngularCorePlugin],
-//   module: { parser: { javascript: { importMeta: false } } }
-// }
+  return {
+    ...webpackConfig,
+    plugins: [...(webpackConfig.plugins ?? []), modifyPrimeNgPlugin, modifyAngularCorePlugin],
+    module: {
+      ...webpackConfig.module,
+      parser: {
+        ...webpackConfig.module.parser,
+        javascript: { ...webpackConfig.module.parser.javascript, importMeta: false }
+      }
+    }
+  }
+}

056434499cdce758a8bf75c11269bcd62efc6fd4
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-02-10 07:50:39 +0100

    feat: stashed changes

--- src/app/remotes/shell-toast/shell-toast.component.bootstrap.ts
+++ src/app/remotes/shell-toast/shell-toast.component.bootstrap.ts
@@ -1,32 +1,32 @@
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { TranslateLoader } from '@ngx-translate/core'
-import { ReplaySubject } from 'rxjs'
+// import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+// import { TranslateLoader } from '@ngx-translate/core'
+// import { ReplaySubject } from 'rxjs'
 
-import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
-import { provideTranslateServiceForRoot } from '@onecx/angular-remote-components'
-import {
-  createTranslateLoader,
-  provideTranslationPathFromMeta,
-  REMOTE_COMPONENT_CONFIG,
-  RemoteComponentConfig
-} from '@onecx/angular-utils'
+// import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
+// import { provideTranslateServiceForRoot } from '@onecx/angular-remote-components'
+// import {
+//   createTranslateLoader,
+//   provideTranslationPathFromMeta,
+//   REMOTE_COMPONENT_CONFIG,
+//   RemoteComponentConfig
+// } from '@onecx/angular-utils'
 
-import { environment } from 'src/environments/environment'
+// import { environment } from 'src/environments/environment'
 
-import { OneCXShellToastComponent } from './shell-toast.component'
-import { provideAnimations } from '@angular/platform-browser/animations'
+// import { OneCXShellToastComponent } from './shell-toast.component'
+// import { provideAnimations } from '@angular/platform-browser/animations'
 
-bootstrapRemoteComponent(OneCXShellToastComponent, 'ocx-shell-toast-component', environment.production, [
-  { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<RemoteComponentConfig>(1) },
-  provideAnimations(),
-  provideTranslateServiceForRoot({
-    isolate: true,
-    loader: {
-      provide: TranslateLoader,
-      useFactory: createTranslateLoader,
-      deps: [HttpClient]
-    }
-  }),
-  provideHttpClient(withInterceptorsFromDi()),
-  provideTranslationPathFromMeta(import.meta.url, 'assets/i18n/')
-])
+// bootstrapRemoteComponent(OneCXShellToastComponent, 'ocx-shell-toast-component', environment.production, [
+//   { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<RemoteComponentConfig>(1) },
+//   provideAnimations(),
+//   provideTranslateServiceForRoot({
+//     isolate: true,
+//     loader: {
+//       provide: TranslateLoader,
+//       useFactory: createTranslateLoader,
+//       deps: [HttpClient]
+//     }
+//   }),
+//   provideHttpClient(withInterceptorsFromDi()),
+//   provideTranslationPathFromMeta(import.meta.url, 'assets/i18n/')
+// ])
--- src/app/remotes/version-info/version-info.component.bootstrap.ts
+++ src/app/remotes/version-info/version-info.component.bootstrap.ts
@@ -1,30 +1,30 @@
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { TranslateLoader } from '@ngx-translate/core'
-import { ReplaySubject } from 'rxjs'
+// import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+// import { TranslateLoader } from '@ngx-translate/core'
+// import { ReplaySubject } from 'rxjs'
 
-import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
-import { provideTranslateServiceForRoot } from '@onecx/angular-remote-components'
-import {
-  createTranslateLoader,
-  provideTranslationPathFromMeta,
-  REMOTE_COMPONENT_CONFIG,
-  RemoteComponentConfig
-} from '@onecx/angular-utils'
+// import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
+// import { provideTranslateServiceForRoot } from '@onecx/angular-remote-components'
+// import {
+//   createTranslateLoader,
+//   provideTranslationPathFromMeta,
+//   REMOTE_COMPONENT_CONFIG,
+//   RemoteComponentConfig
+// } from '@onecx/angular-utils'
 
-import { environment } from 'src/environments/environment'
+// import { environment } from 'src/environments/environment'
 
-import { OneCXVersionInfoComponent } from './version-info.component'
+// import { OneCXVersionInfoComponent } from './version-info.component'
 
-bootstrapRemoteComponent(OneCXVersionInfoComponent, 'ocx-version-info-component', environment.production, [
-  { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<RemoteComponentConfig>(1) },
-  provideTranslateServiceForRoot({
-    isolate: true,
-    loader: {
-      provide: TranslateLoader,
-      useFactory: createTranslateLoader,
-      deps: [HttpClient]
-    }
-  }),
-  provideHttpClient(withInterceptorsFromDi()),
-  provideTranslationPathFromMeta(import.meta.url, 'assets/i18n/')
-])
+// bootstrapRemoteComponent(OneCXVersionInfoComponent, 'ocx-version-info-component', environment.production, [
+//   { provide: REMOTE_COMPONENT_CONFIG, useValue: new ReplaySubject<RemoteComponentConfig>(1) },
+//   provideTranslateServiceForRoot({
+//     isolate: true,
+//     loader: {
+//       provide: TranslateLoader,
+//       useFactory: createTranslateLoader,
+//       deps: [HttpClient]
+//     }
+//   }),
+//   provideHttpClient(withInterceptorsFromDi()),
+//   provideTranslationPathFromMeta(import.meta.url, 'assets/i18n/')
+// ])

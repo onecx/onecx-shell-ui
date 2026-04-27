722d455ce140c080abd81854fe77b280b1c288d6
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-04-27 13:13:06 +0200

    feat: use platform id context while creating elements

--- src/app/shell/utils/styles/angular-material-overwrites.utils.ts
+++ src/app/shell/utils/styles/angular-material-overwrites.utils.ts
@@ -6,8 +6,13 @@ export function ensureMaterialDynamicDataIncludesIntermediateStyleData() {
   // eslint-disable-next-line @typescript-eslint/no-extra-semi
   ;(document as any).createElementFromMaterial = function (context: any, tagName: any, options?: any): HTMLElement {
     const el = document.createElement(tagName, options)
+    // https://github.com/angular/components/blob/v21.2.8/src/cdk/overlay/overlay-container.ts#L33
+    const platformIdAppName = context['this']?._platform?._platformId?.appElementName
+    const platformIdElement = document.getElementsByTagName(platformIdAppName)[0]
     const onecxTrigger = getOnecxTriggerElement()
-    const styleData = onecxTrigger ? getStyleDataOrIntermediateStyleData(onecxTrigger) : null
+    const styleData =
+      (platformIdElement ? getStyleDataOrIntermediateStyleData(platformIdElement) : null) ??
+      (onecxTrigger ? getStyleDataOrIntermediateStyleData(onecxTrigger) : null)
     // Append intermediate data so the isolation does not happen by coincidence
     if (styleData) {
       appendIntermediateStyleData(el, styleData)

fc44f1f532eb285d3cd8c44d518de4dbd6b9b38b
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-05-05 12:04:35 +0200

    fix: angular cdk menu issue

--- src/app/shell/utils/styles/body-overwrites.utils.ts
+++ src/app/shell/utils/styles/body-overwrites.utils.ts
@@ -2,6 +2,7 @@ import { POLYFILL_SCOPE_MODE } from '@onecx/angular-integration-interface'
 import { isCssScopeRuleSupported } from '@onecx/angular-utils'
 import { createNodeList, updateStyleSheets } from 'src/scope-polyfill/polyfill'
 import {
+  appendStyleData,
   findStyleDataWrapper,
   getStyleDataOrIntermediateStyleData,
   markElement,
@@ -111,10 +112,31 @@ function wrapWithStyleData(
   return result
 }
 
+/**
+ * Leave cdk-overlay-container unstyled when adding to the DOM.
+ *
+ * When adding a child to cdk-overlay-container:
+ * - get style data of the appended child
+ * - set style data on the cdk container
+ */
 function overwriteAngularMaterialOverlayContainer(cdkNode: Node) {
   const originalAppendChild = cdkNode.appendChild
   cdkNode.appendChild = function (newChild: Node): any {
-    return wrapWithStyleData(this, newChild, originalAppendChild, undefined)
+    markElement(newChild, 'overwriteAppendChild')
+    if (newChild.nodeType === Node.ELEMENT_NODE && newChild instanceof HTMLElement) {
+      // Get style data
+      const onecxTriggerElement = getOnecxTriggerElement()
+      const triggerElementStyleData = onecxTriggerElement
+        ? getStyleDataOrIntermediateStyleData(onecxTriggerElement)
+        : null
+      const childElementStyleData = getStyleDataOrIntermediateStyleData(newChild)
+      const styleData = childElementStyleData ?? triggerElementStyleData ?? undefined
+      if (styleData) {
+        appendStyleData(this as HTMLElement, styleData)
+      }
+      removeStyleDataRecursive(newChild)
+    }
+    return originalAppendChild.call(this, newChild)
   }
 
   overwriteRemoveChild(cdkNode)
--- src/app/shell/utils/styles/style-data.utils.ts
+++ src/app/shell/utils/styles/style-data.utils.ts
@@ -96,8 +96,8 @@ export function findStyleDataWrapper(element: HTMLElement): HTMLElement | null {
  * Recursively removes style data from an element and its children.
  * @param element HTMLElement to remove style data from.
  */
-export function removeStyleDataRecursive(element: HTMLElement): void {
-  if (element.dataset) {
+export function removeStyleDataRecursive(element: HTMLElement, removeFromCurrent = true): void {
+  if (element.dataset && removeFromCurrent) {
     delete element.dataset[dataStyleIsolationKey]
     delete element.dataset[dataStyleIdKey]
     delete element.dataset[dataNoPortalLayoutStylesKey]

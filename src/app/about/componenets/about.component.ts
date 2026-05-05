776d8c767dc9994e828677ed400616afbdd8e070
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-05-05 11:13:13 +0200

    feat: adjust about page to new config

--- src/app/about/componenets/about.component.ts
+++ src/app/about/componenets/about.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core'
 import { TranslateModule } from '@ngx-translate/core'
 
 interface AngularVersion {
-  name: string,
-  version: string,
-  from: string,
-  eager: boolean,
-  loaded?: number,
+  name: string
+  version: string
+  from: string
+  eager: boolean
+  loaded?: number
+  shareScope?: string
 }
 
 const magicChar = String.fromCodePoint(0x10ffff) // Magic character for preloaders
@@ -16,30 +17,39 @@ const magicChar = String.fromCodePoint(0x10ffff) // Magic character for preloade
   standalone: true,
   imports: [CommonModule, TranslateModule],
   selector: 'ocx-shell-about',
-  templateUrl: './about.component.html',
+  templateUrl: './about.component.html'
 })
 export class AboutComponent implements OnInit {
   supportedAngularVersions: AngularVersion[] = []
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadSupportedVersions()
   }
 
-  private loadSupportedVersions(){
-    try {
-      Object.entries((globalThis as any).onecxWebpackContainer?.default['@angular/core'] || {}).forEach(([version, data]: [string, any]) => {
-        if (data.from.startsWith(magicChar)) {
+  private loadSupportedVersions() {
+    const shellScopeMap = __FEDERATION__?.__INSTANCES__?.find((i) => i.name === 'onecx_shell_ui')?.shareScopeMap
+    if (!shellScopeMap) {
+      console.warn('onecx_shell_ui shareScopeMap not found. Supported Angular versions cannot be determined.')
+      return
+    }
+
+    Object.entries(shellScopeMap).forEach(([scopeName, scopeData]: [string, any]) => {
+      Object.entries(scopeData['@angular/core']).forEach(([version, data]: [string, any]) => {
+        if (this.isAngularVersionBoundary(data)) {
           this.supportedAngularVersions.push({
-            name: "Angular " + version.substring(0, version.indexOf('.')),
+            name: 'Angular ' + version.substring(0, version.indexOf('.')),
             version: version,
             from: data.from.replace(magicChar, ''),
             eager: data['eager'],
             loaded: data['loaded'] || 0,
-          });
+            shareScope: scopeName
+          })
         }
-      });
-    } catch (error) {
-      console.error('Error while accessing onecxWebpackContainer :', error);
-    }
+      })
+    })
+  }
+
+  private isAngularVersionBoundary(data: any): boolean {
+    return data.from.startsWith('onecx_angular_') || (data.from === 'onecx_shell_ui' && data.useIn.length > 0)
   }
 }

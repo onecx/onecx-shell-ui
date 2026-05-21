34297b37bfc00acb1800f2bdd059b7095bc2af81
markuczy <129275100+markuczy@users.noreply.github.com>
2026-05-21 09:32:54 +0200

    feat: share shell's federation instance globally (#929)
    * feat: share shell's federation instance globally
    
    * fix: comment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ensureProperty } from '@onecx/accelerator'
 import {
   angular18Preloader,
   angular19Preloader,
@@ -6,6 +7,9 @@ import {
   ensurePreloaderModuleLoaded,
   loadPreloaderModule
 } from './app/shell/utils/preloader.utils'
+
+// Make shell federation instance is available globally
+ensureProperty(globalThis, ['onecxFederationInstance'], __FEDERATION__.__INSTANCES__[0])
 
 window['onecxPreloaders'] ??= {}
 const preloaders = [angular18Preloader, angular19Preloader, angular20Preloader, angular21Preloader]

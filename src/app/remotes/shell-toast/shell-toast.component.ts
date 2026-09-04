e23acf47816285b4921ed5494242eb9917d17a81
Prathamesh Malavi <239937892+prathamMalavi@users.noreply.github.com>
2026-09-04 17:21:40 +0530

    fix: v3 shell-toast not showing up (#970)
    * fix: v3 shell-toast not showing up
    
    * fix: added comment

--- src/app/remotes/shell-toast/shell-toast.component.ts
+++ src/app/remotes/shell-toast/shell-toast.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject, Input, OnInit } from '@angular/core'
+import { Component, inject, Input, OnInit, ChangeDetectorRef } from '@angular/core'
 import { UntilDestroy } from '@ngneat/until-destroy'
 import { ReplaySubject } from 'rxjs'
 
@@ -29,13 +29,19 @@ export class OneCXShellToastComponent implements ocxRemoteComponent, ocxRemoteWe
   private readonly primengConfig: PrimeNG = inject(PrimeNG)
   private readonly messageService = inject(MessageService)
   private readonly portalMessageService = inject(PortalMessageService)
+  private readonly cdr = inject(ChangeDetectorRef)
 
   @Input() set ocxRemoteComponentConfig(rcConfig: RemoteComponentConfig) {
     this.ocxInitRemoteComponent(rcConfig)
   }
 
   constructor() {
-    this.portalMessageService.message$.subscribe((message: Message) => this.messageService.add(message))
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      this.messageService.add(message)
+      // Primng fails to trigger change detection automatically and which cuses the messages to get stacked and pushed once the change detection triggers
+      // so we use setTimeout to manually trigger it
+      setTimeout(() => this.cdr.detectChanges())
+    })
   }
 
   ngOnInit() {
